d11c7765e5709f8dd277c35d79f74fd1416041af
Ruben Bustos <ruben_b_r_@hotmail.com>
2017-11-07 17:30:56 +0100

    fase 1 done with bugs

--- minishell/minishell.h
+++ minishell/minishell.h
@@ -2,6 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+
+#define COMMAND_LINE_SIZE 1024
+#define PARAM_SIZE 3
 
 //MINISHELL FUNCTIONS
+void print_prompt();
 char *read_line(char *line);
+int execute_line(char *line);
+int parse_args(char **args, char *line);
+int check_internal(char **args);
+int internal_cd(char **args);
+int internal_export(char **args);
+int internal_source(char **args);
+int internal_jobs(char **args);
+int external_command(char **args);
